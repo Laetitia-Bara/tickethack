b712a74fc1ad3c77fce161c103c3aab9864060bd
Laetitia <bara.laetitia@gmail.com>
2026-01-28 09:09:41 +0100

    Ajout frontend

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+const BACKEND_URL = "https://tickethack-backend-liart.vercel.app";
+
+fetch('BACKEND_URL')
+	.then(response => response.json())
+	.then(data => {
+
+document.querySelector("#btn-search").addEventListener("click", function () {
+ const departure = document.querySelector('#departure').value;
+ const arrival = document.querySelector('#arrival').value;
+ const date = document.querySelector('#date').value;
+
+})})
+
